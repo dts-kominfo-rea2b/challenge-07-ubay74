d35a7f252fcc4ff86a34a37cdcb1ced8a466000f
ubay74 <105720990+ubay74@users.noreply.github.com>
2022-06-17 07:27:19 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reaksi) => {
+  return Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    .then((respon) => {
+      const [IXX, VGC] = respon;
+      return [...IXX, ...VGC].filter((movie) => movie.hasil === reaksi).length;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+};
 
 module.exports = {
   promiseOutput,
